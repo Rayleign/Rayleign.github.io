1c913b0e766a68343e06d0cf08c88b4e7968050f
Rayleign Liu <rayleignliu@anjuke.com>
2014-07-23 16:17:17 +0800

    add Slides

---
+++ slides/AppPackageTool/slide_config.js
@@ -0,0 +1,38 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    //title: '移动APP打包工具<br>设计方案Review',
+    title: 'Good things come in <br/>small packages',
+    subtitle: 'APP打包项目设计Review',
+    //eventInfo: {
+    //  title: 'Google I/O',
+    //  date: '2014-01-04'
+   // },
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: '',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: '刘瑞丽',
+    company: 'Rayleign<br/>经纪人事业部 移动组',
+    mail: 'rayleignliu@anjuke.com'
+  }/*, {
+    name: 'Second Name',
+    company: 'Job Title, Google',
+    gplus: 'http://plus.google.com/1234567890',
+    twitter: '@yourhandle',
+    www: 'http://www.you.com',
+    github: 'http://github.com/you'
+  }*/]
+};
+
